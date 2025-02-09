4b9037897fcdfbba9baed62bf14216505221355a
bygxs <biniyamyohannes@icloud.com>
2025-02-09 10:25:46 +0100

    imported signOut

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; // Import Firebase Authentication
+import { signOut } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // Import Firestore
 import { getStorage } from "firebase/storage"; // Import Firebase Storage
 
